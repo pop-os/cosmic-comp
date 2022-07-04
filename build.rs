d182d5b3880a8b2c0584f26a5cc54379ca1ed9a9
Victoria Brekenfeld <victoria@system76.com>
2022-07-04 15:24:50 +0200

    deps: Update to wayland-rs.0.30

--- build.rs
+++ build.rs
@@ -1,8 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-only
-extern crate wayland_scanner;
-
-use std::{env, path::PathBuf, process::Command};
-use wayland_scanner::{generate_code, Side};
+use std::process::Command;
 
 fn main() {
     if let Some(output) = Command::new("git")
@@ -13,16 +10,4 @@ fn main() {
         let git_hash = String::from_utf8(output.stdout).unwrap();
         println!("cargo:rustc-env=GIT_HASH={}", git_hash);
     }
-
-    let dest = PathBuf::from(&env::var("OUT_DIR").unwrap());
-    // Location of the xml file, relative to the `Cargo.toml`
-    let drm_protocol_file = "resources/wayland-drm.xml";
-    let ext_workspace_protocol_file = "resources/ext-workspace-unstable-v1.xml";
-    // Target directory for the generate files
-    generate_code(drm_protocol_file, &dest.join("wl_drm.rs"), Side::Server);
-    generate_code(
-        ext_workspace_protocol_file,
-        &dest.join("ext_workspace.rs"),
-        Side::Server,
-    );
 }
