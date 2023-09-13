pop-os/cosmic-comp

commit b818a68a91c83cd3ab5da79cebb5f144cb2feeee

tiling: Properly handle resizing using a ptr device

src/shell/layout/tiling/mod.rs
@@ -10,7 +10,7 @@ use crate::{
             CosmicMapped, CosmicMappedRenderElement, CosmicStack, CosmicWindow,
         },
         focus::{
-            target::{KeyboardFocusTarget, WindowGroup},
+            target::{KeyboardFocusTarget, PointerFocusTarget, WindowGroup},
             FocusDirection, FocusStackMut,
         },
         grabs::ResizeEdge,
@@ -34,7 +34,7 @@ use smithay::{
         ImportAll, ImportMem, Renderer,
     },
     desktop::{layer_map_for_output, space::SpaceElement, PopupKind},
-    input::{pointer::GrabStartData as PointerGrabStartData, Seat},
+    input::Seat,
     output::Output,
     reexports::wayland_server::Client,
     utils::{IsAlive, Logical, Point, Rectangle, Scale},
@@ -1389,60 +1389,6 @@ impl TilingLayout {
         clients
     }
 
-    pub fn resize_request(
-        &self,
-        mapped: &CosmicMapped,
-        _seat: &Seat<State>,
-        start_data: PointerGrabStartData<State>,
-        edges: ResizeEdge,
-    ) -> Option<ResizeForkGrab> {
-        let (output, mut node_id) = self.queues.iter().find_map(|(output, queue)| {
-            let tree = &queue.trees.back().unwrap().0;
-            let root_id = tree.root_node_id()?;
-            tree.traverse_pre_order_ids(root_id)
-                .unwrap()
-                .find(|id| tree.get(id).unwrap().data().is_mapped(Some(mapped)))
-                .map(|id| (&output.output, id))
-        })?;
-
-        let queue = self.queues.get(output).unwrap();
-        let tree = &queue.trees.back().unwrap().0;
-        while let Some(group_id) = tree.get(&node_id).unwrap().parent() {
-            let orientation = tree.get(group_id).unwrap().data().orientation();
-            if !((orientation == Orientation::Vertical
-                && (edges.contains(ResizeEdge::LEFT) || edges.contains(ResizeEdge::RIGHT)))
-                || (orientation == Orientation::Horizontal
-                    && (edges.contains(ResizeEdge::TOP) || edges.contains(ResizeEdge::BOTTOM))))
-            {
-                node_id = group_id.clone();
-                continue;
-            }
-
-            let node_idx = tree
-                .children_ids(group_id)
-                .unwrap()
-                .position(|id| id == &node_id)
-                .unwrap();
-            let idx = match edges {
-                x if x.intersects(ResizeEdge::TOP_LEFT) => node_idx.checked_sub(1)?,
-                _ => node_idx,
-            };
-            if idx > tree.get(&group_id).unwrap().data().len() {
-                return None;
-            }
-
-            return Some(ResizeForkGrab::new(
-                start_data,
-                group_id.clone(),
-                output,
-                tree.get(&group_id).unwrap().data(),
-                idx,
-            ));
-        }
-
-        None
-    }
-
     pub fn possible_resizes(tree: &Tree<Data>, mut node_id: NodeId) -> ResizeEdge {
         let mut edges = ResizeEdge::empty();
 
@@ -1533,6 +1479,15 @@ impl TilingLayout {
                         (node_idx, other_idx)
                     } else {
                         (other_idx, node_idx)
+                    };
+
+                    if sizes[shrink_idx] + sizes[grow_idx]
+                        < match orientation {
+                            Orientation::Vertical => 720,
+                            Orientation::Horizontal => 480,
+                        }
+                    {
+                        return true;
                     };
 
                     let old_size = sizes[shrink_idx];
@@ -1784,6 +1739,102 @@ impl TilingLayout {
         }
 
         None
+    }
+
+    pub fn element_under(
+        &self,
+        location: Point<f64, Logical>,
+    ) -> Option<(PointerFocusTarget, Point<i32, Logical>)> {
+        self.queues.iter().find_map(|(output_data, queue)| {
+            let tree = &queue.trees.back().unwrap().0;
+            let root = tree.root_node_id()?;
+            let location = (location - output_data.location.to_f64()).to_i32_round();
+
+            let mut result = None;
+            let mut lookup = Some(root.clone());
+            while let Some(node) = lookup {
+                let data = tree.get(&node).unwrap().data();
+                if data.geometry().contains(location) {
+                    result = Some(node.clone());
+                }
+
+                lookup = None;
+                if result.is_some() && data.is_group() {
+                    for child_id in tree.children_ids(&node).unwrap() {
+                        if tree
+                            .get(child_id)
+                            .unwrap()
+                            .data()
+                            .geometry()
+                            .contains(location)
+                        {
+                            lookup = Some(child_id.clone());
+                            break;
+                        }
+                    }
+                }
+            }
+
+            match result.map(|id| (id.clone(), tree.get(&id).unwrap().data().clone())) {
+                Some((
+                    _,
+                    Data::Mapped {
+                        mapped,
+                        last_geometry,
+                    },
+                )) => {
+                    let test_point = location.to_f64() - last_geometry.loc.to_f64()
+                        + mapped.geometry().loc.to_f64();
+                    mapped.is_in_input_region(&test_point).then(|| {
+                        (
+                            mapped.clone().into(),
+                            last_geometry.loc - output_data.location - mapped.geometry().loc,
+                        )
+                    })
+                }
+                Some((
+                    id,
+                    Data::Group {
+                        orientation,
+                        last_geometry,
+                        ..
+                    },
+                )) => {
+                    let idx = tree
+                        .children(&id)
+                        .unwrap()
+                        .position(|node| {
+                            let data = node.data();
+                            match orientation {
+                                Orientation::Vertical => location.x < data.geometry().loc.x,
+                                Orientation::Horizontal => location.y < data.geometry().loc.y,
+                            }
+                        })
+                        .and_then(|x| x.checked_sub(1))?;
+                    Some((
+                        ResizeForkTarget {
+                            node: id.clone(),
+                            output: output_data.output.downgrade(),
+                            left_up_idx: idx,
+                            orientation,
+                        }
+                        .into(),
+                        last_geometry.loc - output_data.location
+                            + tree
+                                .children(&id)
+                                .unwrap()
+                                .skip(idx)
+                                .next()
+                                .map(|node| {
+                                    let geo = node.data().geometry();
+                                    geo.loc + geo.size
+                                })
+                                .unwrap(),
+                    ))
+                }
+                _ => None,
+            }
+        })
     }
 
     pub fn mapped(

src/input/mod.rs
@@ -13,7 +13,7 @@ use crate::{
     }, // shell::grabs::SeatMoveGrabState
     state::Common,
     utils::prelude::*,
-    wayland::{handlers::screencopy::ScreencopySessions, protocols::screencopy::Session},
+    wayland::{handlers::screencopy::ScreencopySessions, protocols::screencopy::Session}, backend::render::cursor::CursorState,
 };
 use calloop::{timer::Timer, RegistrationToken};
 use cosmic_protocols::screencopy::v1::server::zcosmic_screencopy_session_v1::InputType;
@@ -141,6 +141,7 @@ pub fn add_seat(
     userdata.insert_if_missing(Devices::default);
     userdata.insert_if_missing(SupressedKeys::default);
     userdata.insert_if_missing(SeatMoveGrabState::default);
+    userdata.insert_if_missing(CursorState::default);
     userdata.insert_if_missing(|| ActiveOutput(RefCell::new(output.clone())));
     userdata.insert_if_missing(|| RefCell::new(CursorImageStatus::Default));
 
@@ -664,10 +665,10 @@ impl State {
                                         {
                                             under = Some(layer.clone().into());
                                         }
-                                    } else if let Some((window, _)) =
+                                    } else if let Some((target, _)) =
                                         workspace.element_under(relative_pos)
                                     {
-                                        under = Some(window.clone().into());
+                                        under = Some(target);
                                     } else if let Some(layer) = layers
                                         .layer_under(WlrLayer::Bottom, pos)
                                         .or_else(|| layers.layer_under(WlrLayer::Background, pos))
@@ -685,8 +686,7 @@ impl State {
                                         }
                                     };
                                 }
-
-                                Common::set_focus(self, under.as_ref(), seat, Some(serial));
+                                Common::set_focus(self, under.and_then(|target| target.try_into().ok()).as_ref(), seat, Some(serial));
                             }
                         };
                         seat.get_pointer().unwrap().button(
@@ -1347,9 +1347,9 @@ impl State {
             {
                 return Some((or.clone().into(), or.geometry().loc));
             }
-            if let Some((mapped, loc)) = workspace.element_under(relative_pos) {
+            if let Some((target, loc)) = workspace.element_under(relative_pos) {
                 return Some((
-                    mapped.clone().into(),
+                    target,
                     loc + (global_pos - relative_pos).to_i32_round(),
                 ));
             }

src/backend/render/cursor.rs
@@ -37,6 +37,24 @@ use xcursor::{
 
 static FALLBACK_CURSOR_DATA: &[u8] = include_bytes!("../../../resources/cursor.rgba");
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum CursorShape {
+    Default,
+    ColResize,
+    RowResize,
+}
+
+impl ToString for CursorShape {
+    fn to_string(&self) -> String {
+        match self {
+            CursorShape::Default => "default",
+            CursorShape::ColResize => "col-resize",
+            CursorShape::RowResize => "row-resize",
+        }
+        .to_string()
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct Cursor {
     icons: Vec<Image>,
@@ -44,17 +62,8 @@ pub struct Cursor {
 }
 
 impl Cursor {
-    pub fn load() -> Cursor {
-        let name = std::env::var("XCURSOR_THEME")
-            .ok()
-            .unwrap_or_else(|| "default".into());
-        let size = std::env::var("XCURSOR_SIZE")
-            .ok()
-            .and_then(|s| s.parse().ok())
-            .unwrap_or(24);
-
-        let theme = CursorTheme::load(&name);
-        let icons = load_icon(&theme)
+    pub fn load(theme: &CursorTheme, shape: CursorShape, size: u32) -> Cursor {
+        let icons = load_icon(&theme, shape)
             .map_err(|err| warn!(?err, "Unable to load xcursor, using fallback cursor"))
             .unwrap_or_else(|_| {
                 vec![Image {
@@ -75,12 +84,6 @@ impl Cursor {
     pub fn get_image(&self, scale: u32, millis: u32) -> Image {
         let size = self.size * scale;
         frame(millis, size, &self.icons)
-    }
-}
-
-impl Default for Cursor {
-    fn default() -> Cursor {
-        Cursor::load()
     }
 }
 
@@ -120,8 +123,10 @@ enum Error {
     Parse,
 }
 
-fn load_icon(theme: &CursorTheme) -> Result<Vec<Image>, Error> {
-    let icon_path = theme.load_icon("default").ok_or(Error::NoDefaultCursor)?;
+fn load_icon(theme: &CursorTheme, shape: CursorShape) -> Result<Vec<Image>, Error> {
+    let icon_path = theme
+        .load_icon(&shape.to_string())
+        .ok_or(Error::NoDefaultCursor)?;
     let mut cursor_file = std::fs::File::open(&icon_path)?;
     let mut cursor_data = Vec::new();
     cursor_file.read_to_end(&mut cursor_data)?;
@@ -196,15 +201,50 @@ where
 }
 
 pub struct CursorState {
-    pub cursor: Cursor,
+    current_cursor: RefCell<CursorShape>,
+    pub cursors: HashMap<CursorShape, Cursor>,
     current_image: RefCell<Option<Image>>,
     image_cache: RefCell<HashMap<(TypeId, usize), Vec<(Image, Box<dyn Any + 'static>)>>>,
 }
 
+impl CursorState {
+    pub fn set_shape(&self, shape: CursorShape) {
+        *self.current_cursor.borrow_mut() = shape;
+    }
+}
+
+pub fn load_cursor_theme() -> (CursorTheme, u32) {
+    let name = std::env::var("XCURSOR_THEME")
+        .ok()
+        .unwrap_or_else(|| "default".into());
+    let size = std::env::var("XCURSOR_SIZE")
+        .ok()
+        .and_then(|s| s.parse().ok())
+        .unwrap_or(24);
+    (CursorTheme::load(&name), size)
+}
+
 impl Default for CursorState {
     fn default() -> CursorState {
+        let (theme, size) = load_cursor_theme();
         CursorState {
-            cursor: Cursor::default(),
+            current_cursor: RefCell::new(CursorShape::Default),
+            cursors: {
+                let mut map = HashMap::new();
+                map.insert(
+                    CursorShape::Default,
+                    Cursor::load(&theme, CursorShape::Default, size),
+                );
+                map.insert(
+                    CursorShape::ColResize,
+                    Cursor::load(&theme, CursorShape::ColResize, size),
+                );
+                map.insert(
+                    CursorShape::RowResize,
+                    Cursor::load(&theme, CursorShape::RowResize, size),
+                );
+                map
+            },
             current_image: RefCell::new(None),
             image_cache: RefCell::new(HashMap::new()),
         }
@@ -247,12 +287,15 @@ where
         let integer_scale = scale.x.max(scale.y).ceil() as u32;
 
         let seat_userdata = seat.user_data();
-        seat_userdata.insert_if_missing(CursorState::default);
         let state = seat_userdata.get::<CursorState>().unwrap();
-        let frame = state.cursor.get_image(
-            integer_scale,
-            Into::<Duration>::into(time).as_millis() as u32,
-        );
+        let frame = state
+            .cursors
+            .get(&*state.current_cursor.borrow())
+            .unwrap()
+            .get_image(
+                integer_scale,
+                Into::<Duration>::into(time).as_millis() as u32,
+            );
 
         let mut cache = state.image_cache.borrow_mut();
         let pointer_images = cache

src/shell/focus/target.rs
@@ -1,7 +1,7 @@
 use std::sync::Weak;
 
 use crate::{
-    shell::{element::CosmicMapped, CosmicSurface},
+    shell::{element::CosmicMapped, layout::tiling::ResizeForkTarget, CosmicSurface},
     utils::prelude::*,
     wayland::handlers::xdg_shell::popup::get_popup_toplevel,
 };
@@ -28,6 +28,7 @@ pub enum PointerFocusTarget {
     LayerSurface(LayerSurface),
     Popup(PopupKind),
     OverrideRedirect(X11Surface),
+    ResizeFork(ResizeForkTarget),
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -39,6 +40,7 @@ pub enum KeyboardFocusTarget {
     Popup(PopupKind),
 }
 
+// TODO: This should be TryFrom, but PopupGrab needs to be able to convert. Fix this in smithay
 impl From<KeyboardFocusTarget> for PointerFocusTarget {
     fn from(target: KeyboardFocusTarget) -> Self {
         match target {
@@ -47,6 +49,19 @@ impl From<KeyboardFocusTarget> for PointerFocusTarget {
             KeyboardFocusTarget::LayerSurface(layer) => PointerFocusTarget::LayerSurface(layer),
             KeyboardFocusTarget::Popup(popup) => PointerFocusTarget::Popup(popup),
             _ => unreachable!("A window grab cannot start a popup grab"),
+        }
+    }
+}
+
+impl TryFrom<PointerFocusTarget> for KeyboardFocusTarget {
+    type Error = ();
+    fn try_from(target: PointerFocusTarget) -> Result<Self, Self::Error> {
+        match target {
+            PointerFocusTarget::Element(mapped) => Ok(KeyboardFocusTarget::Element(mapped)),
+            PointerFocusTarget::Fullscreen(surf) => Ok(KeyboardFocusTarget::Fullscreen(surf)),
+            PointerFocusTarget::LayerSurface(layer) => Ok(KeyboardFocusTarget::LayerSurface(layer)),
+            PointerFocusTarget::Popup(popup) => Ok(KeyboardFocusTarget::Popup(popup)),
+            _ => Err(()),
         }
     }
 }
@@ -85,6 +100,7 @@ impl IsAlive for PointerFocusTarget {
             PointerFocusTarget::LayerSurface(l) => l.alive(),
             PointerFocusTarget::Popup(p) => p.alive(),
             PointerFocusTarget::OverrideRedirect(s) => s.alive(),
+            PointerFocusTarget::ResizeFork(f) => f.alive(),
         }
     }
 }
@@ -109,6 +125,7 @@ impl PointerTarget<State> for PointerFocusTarget {
             PointerFocusTarget::LayerSurface(l) => PointerTarget::enter(l, seat, data, event),
             PointerFocusTarget::Popup(p) => PointerTarget::enter(p.wl_surface(), seat, data, event),
             PointerFocusTarget::OverrideRedirect(s) => PointerTarget::enter(s, seat, data, event),
+            PointerFocusTarget::ResizeFork(f) => PointerTarget::enter(f, seat, data, event),
         }
     }
     fn motion(&self, seat: &Seat<State>, data: &mut State, event: &MotionEvent) {
@@ -120,6 +137,7 @@ impl PointerTarget<State> for PointerFocusTarget {
                 PointerTarget::motion(p.wl_surface(), seat, data, event)
             }
             PointerFocusTarget::OverrideRedirect(s) => PointerTarget::motion(s, seat, data, event),
+            PointerFocusTarget::ResizeFork(f) => PointerTarget::motion(f, seat, data, event),
         }
     }
     fn relative_motion(&self, seat: &Seat<State>, data: &mut State, event: &RelativeMotionEvent) {
@@ -137,6 +155,9 @@ impl PointerTarget<State> for PointerFocusTarget {
             PointerFocusTarget::OverrideRedirect(s) => {
                 PointerTarget::relative_motion(s, seat, data, event)
             }
+            PointerFocusTarget::ResizeFork(f) => {
+                PointerTarget::relative_motion(f, seat, data, event)
+            }
         }
     }
     fn button(&self, seat: &Seat<State>, data: &mut State, event: &ButtonEvent) {
@@ -148,6 +169,7 @@ impl PointerTarget<State> for PointerFocusTarget {
                 PointerTarget::button(p.wl_surface(), seat, data, event)
             }
             PointerFocusTarget::OverrideRedirect(s) => PointerTarget::button(s, seat, data, event),
+            PointerFocusTarget::ResizeFork(f) => PointerTarget::button(f, seat, data, event),
         }
     }
     fn axis(&self, seat: &Seat<State>, data: &mut State, frame: AxisFrame) {
@@ -157,6 +179,7 @@ impl PointerTarget<State> for PointerFocusTarget {
             PointerFocusTarget::LayerSurface(l) => PointerTarget::axis(l, seat, data, frame),
             PointerFocusTarget::Popup(p) => PointerTarget::axis(p.wl_surface(), seat, data, frame),
             PointerFocusTarget::OverrideRedirect(s) => PointerTarget::axis(s, seat, data, frame),
+            PointerFocusTarget::ResizeFork(f) => PointerTarget::axis(f, seat, data, frame),
         }
     }
     fn leave(&self, seat: &Seat<State>, data: &mut State, serial: Serial, time: u32) {
@@ -172,6 +195,7 @@ impl PointerTarget<State> for PointerFocusTarget {
             PointerFocusTarget::OverrideRedirect(s) => {
                 PointerTarget::leave(s, seat, data, serial, time)
             }
+            PointerFocusTarget::ResizeFork(f) => PointerTarget::leave(f, seat, data, serial, time),
         }
     }
 }
@@ -290,6 +314,9 @@ impl WaylandFocus for PointerFocusTarget {
             PointerFocusTarget::OverrideRedirect(s) => {
                 return s.wl_surface();
             }
+            PointerFocusTarget::ResizeFork(_) => {
+                return None;
+            }
         })
     }
     fn same_client_as(&self, object_id: &ObjectId) -> bool {
@@ -299,6 +326,7 @@ impl WaylandFocus for PointerFocusTarget {
             PointerFocusTarget::LayerSurface(l) => l.wl_surface().id().same_client_as(object_id),
             PointerFocusTarget::Popup(p) => p.wl_surface().id().same_client_as(object_id),
             PointerFocusTarget::OverrideRedirect(s) => WaylandFocus::same_client_as(s, object_id),
+            PointerFocusTarget::ResizeFork(_) => false,
         }
     }
 }
@@ -330,6 +358,12 @@ impl From<PopupKind> for PointerFocusTarget {
 impl From<X11Surface> for PointerFocusTarget {
     fn from(s: X11Surface) -> Self {
         PointerFocusTarget::OverrideRedirect(s)
+    }
+}
+
+impl From<ResizeForkTarget> for PointerFocusTarget {
+    fn from(f: ResizeForkTarget) -> Self {
+        PointerFocusTarget::ResizeFork(f)
     }
 }
 

commit 5df26c231742e1fcd907f74d58cba5245dcfb113

Fix `TilingLayout::element_under` position with global workspaces
I didn't see any issue with how mouse events were handled, but a bisect
showed b818a68a91c83cd3ab5da79cebb5f144cb2feeee caused the issue.
Reverting the definition of `element_under` to the version before that
change fixed the behavior.

Comparing what both versions return, the right element is returned, but
the location returned is wrong. This makes the return value match the
position that was returned by the previous implementation. It seems to
be working correctly now.

Fixes https://github.com/pop-os/cosmic-comp/issues/161.

src/shell/layout/tiling/mod.rs
@@ -3055,7 +3055,7 @@ impl TilingLayout {
                         mapped.is_in_input_region(&test_point).then(|| {
                             (
                                 mapped.clone().into(),
-                                last_geometry.loc - output_data.location - mapped.geometry().loc,
+                                last_geometry.loc + output_data.location - mapped.geometry().loc,
                             )
                         })
                     }
@@ -3086,7 +3086,8 @@ impl TilingLayout {
                                 orientation,
                             }
                             .into(),
-                            last_geometry.loc - output_data.location
+                            last_geometry.loc
+                                + output_data.location
                                 + tree
                                     .children(&id)
                                     .unwrap()