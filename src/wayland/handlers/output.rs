80a86e0d04389f3de34b806dbc52d8998ef1fd56
Victoria Brekenfeld <github@drakulix.de>
2024-02-01 15:38:05 +0100

    chore: Update smithay

--- src/wayland/handlers/output.rs
+++ src/wayland/handlers/output.rs
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 use crate::state::State;
-use smithay::delegate_output;
+use smithay::{delegate_output, wayland::output::OutputHandler};
+
+impl OutputHandler for State {}
 
 delegate_output!(State);
