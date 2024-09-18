a4321b8ea45fdb9e74da7623a55c21b8749a2156
Ian Douglas Scott <idscott@system76.com>
2024-09-18 12:55:36 -0700

    Run `cargo fmt`

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -264,7 +264,9 @@ impl Config {
                                     .shell
                                     .write()
                                     .unwrap()
-                                    .update_tiling_exceptions(state.common.config.tiling_exceptions.iter());
+                                    .update_tiling_exceptions(
+                                        state.common.config.tiling_exceptions.iter(),
+                                    );
                             }
                             _ => (),
                         }
