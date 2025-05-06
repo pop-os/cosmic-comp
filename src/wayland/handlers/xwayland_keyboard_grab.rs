ad6f219d8c3052b12c64b2a3493bcec39cef1eed
Ian Douglas Scott <idscott@system76.com>
2025-05-05 19:40:56 -0700

    Improved handling of XWayland grabs
    A solution for https://github.com/Smithay/smithay/issues/1714.
    
    With this, the lock screen is able to get keyboard focus normally, but
    focus then reverts to the XWayland grab surface. This can be tested with
    the example client from the issue.
    
    Adding a new variant of `KeyboardFocusTarget` is annoying. Maybe it
    could map to a different variant of the enum. But it presumably needs to
    handle any `wl_surface` XWayland uses. (Override redirect surfaces?
    Subsurfaces?) This seems as good as anything for now.

--- src/wayland/handlers/xwayland_keyboard_grab.rs
+++ src/wayland/handlers/xwayland_keyboard_grab.rs
@@ -2,11 +2,26 @@
 
 use crate::{shell::focus::target::KeyboardFocusTarget, state::State};
 use smithay::{
-    delegate_xwayland_keyboard_grab, reexports::wayland_server::protocol::wl_surface::WlSurface,
-    wayland::xwayland_keyboard_grab::XWaylandKeyboardGrabHandler,
+    delegate_xwayland_keyboard_grab,
+    input::Seat,
+    reexports::wayland_server::protocol::wl_surface::WlSurface,
+    wayland::xwayland_keyboard_grab::{XWaylandKeyboardGrab, XWaylandKeyboardGrabHandler},
 };
+use std::sync::Mutex;
+
+#[derive(Default)]
+pub struct XWaylandGrabSeatData {
+    pub grab: Mutex<Option<(WlSurface, XWaylandKeyboardGrab<State>)>>,
+}
 
 impl XWaylandKeyboardGrabHandler for State {
+    fn grab(&mut self, surface: WlSurface, seat: Seat<Self>, grab: XWaylandKeyboardGrab<Self>) {
+        let data = seat
+            .user_data()
+            .get_or_insert(XWaylandGrabSeatData::default);
+        *data.grab.lock().unwrap() = Some((surface, grab));
+    }
+
     fn keyboard_focus_for_xsurface(&self, surface: &WlSurface) -> Option<KeyboardFocusTarget> {
         let element = self
             .common
