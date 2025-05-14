70e16e2f3a0d963a0f5e9f3d2ae6f1d66c167e52
Ian Douglas Scott <idscott@system76.com>
2025-05-14 16:21:57 -0700

    WIP `org.freedesktop.a11y.Manager` dbus protocol
    unused code
    
    url
    
    unichar
    
    store set grabs
    
    KeyGrab
    
    key grabs
    
    virtual_mods
    
    Fix deadlock

--- src/dbus/mod.rs
+++ src/dbus/mod.rs
@@ -6,6 +6,7 @@ use futures_util::stream::StreamExt;
 use std::collections::HashMap;
 use zbus::blocking::{fdo::DBusProxy, Connection};
 
+pub mod a11y_keyboard_monitor;
 mod name_owners;
 mod power;
 
