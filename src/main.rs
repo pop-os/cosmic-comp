4cf77b3654a16082ad42ee617ab9154f5e0b83e2
Yureka <yuka@yuka.dev>
2025-09-29 17:34:30 +0200

    Add hooks for custom window decorations
    This is a first use of the new hooks system, which allows customizing
    cosmic-comp at compile-time.
    In this case, the view() function of CosmicWindow / CosmicStack is
    hooked and the hook can change what is rendered as the header bar.
    
    Signed-off-by: Yureka <yuka@yuka.dev>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 fn main() {
-    if let Err(err) = cosmic_comp::run() {
+    if let Err(err) = cosmic_comp::run(Default::default()) {
         tracing::error!("Error occured in main(): {}", err);
         std::process::exit(1);
     }
