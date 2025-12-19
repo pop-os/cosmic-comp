edc4a5fe7f8e33d5e2ee6a66496cb2d1b0310c48
Victoria Brekenfeld <victoria@system76.com>
2025-12-19 18:49:22 +0100

    kms: Track active clients instead of active buffers

--- src/wayland/handlers/dmabuf.rs
+++ src/wayland/handlers/dmabuf.rs
@@ -1,13 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
-use crate::state::{BackendData, State};
+use crate::state::State;
 use smithay::{
     backend::allocator::dmabuf::Dmabuf,
     delegate_dmabuf,
-    reexports::wayland_server::Resource,
     wayland::dmabuf::{DmabufGlobal, DmabufHandler, DmabufState, ImportNotifier},
 };
-use tracing::warn;
 
 impl DmabufHandler for State {
     fn dmabuf_state(&mut self) -> &mut DmabufState {
@@ -20,34 +18,13 @@ impl DmabufHandler for State {
         dmabuf: Dmabuf,
         import_notifier: ImportNotifier,
     ) {
-        match self
-            .backend
-            .dmabuf_imported(import_notifier.client(), global, dmabuf)
-        {
+        let client = import_notifier.client();
+        match self.backend.dmabuf_imported(client.clone(), global, dmabuf) {
             Err(err) => {
                 tracing::debug!(?err, "dmabuf import failed");
                 import_notifier.failed()
             }
-            Ok(Some(node)) => {
-                // kms backend
-                let Ok(buffer) = import_notifier.successful::<State>() else {
-                    return;
-                };
-
-                if let BackendData::Kms(kms_state) = &mut self.backend {
-                    if let Some(device) = kms_state
-                        .drm_devices
-                        .values_mut()
-                        .find(|dev| dev.inner.render_node == node)
-                    {
-                        device.inner.active_buffers.insert(buffer.downgrade());
-                    }
-                    if let Err(err) = kms_state.refresh_used_devices() {
-                        warn!(?err, "Failed to init devices.");
-                    };
-                }
-            }
-            Ok(None) => {
+            Ok(_) => {
                 let _ = import_notifier.successful::<State>();
             }
         }
