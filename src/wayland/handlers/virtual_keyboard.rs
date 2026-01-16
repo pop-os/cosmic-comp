6618f59272d4e001526bf73c66aecad0b9e122c3
Ian Douglas Scott <idscott@system76.com>
2026-01-15 17:21:45 -0800

    WIP virtual keyboard

--- src/wayland/handlers/virtual_keyboard.rs
+++ src/wayland/handlers/virtual_keyboard.rs
@@ -2,5 +2,35 @@
 
 use crate::state::State;
 use smithay::delegate_virtual_keyboard_manager;
+use smithay::input::keyboard::xkb::ModMask;
+use smithay::utils::SERIAL_COUNTER;
+use smithay::input::keyboard::KeyboardHandle;
+use smithay::backend::input::KeyState;
+use smithay::wayland::virtual_keyboard::VirtualKeyboardHandler;
+use smithay::input::keyboard::Keycode;
+use smithay::input::keyboard::FilterResult;
+
+impl VirtualKeyboardHandler for State {
+    fn on_keyboard_event(
+        &mut self,
+        keycode: Keycode,
+        state: KeyState,
+        time: u32,
+        keyboard: KeyboardHandle<Self>,
+    ) {
+        let serial = SERIAL_COUNTER.next_serial();
+        keyboard.input(self, keycode, state, serial, time, |_, _, _| {
+            FilterResult::Forward::<bool>
+        });
+    }
+    fn on_keyboard_modifiers(
+        &mut self,
+        _depressed_mods: ModMask,
+        _latched_mods: ModMask,
+        _locked_mods: ModMask,
+        _keyboard: KeyboardHandle<Self>,
+    ) {
+    }
+}
 
 delegate_virtual_keyboard_manager!(State);
