dfec5868a063904d27cbef4835ad26f65ab72efa
Ilia Malanin <iliam@pastila.org>
2026-02-24 17:40:31 +0100

    shell: Preserve fullscreen state when moving window between workspaces

--- src/shell/workspace.rs
+++ src/shell/workspace.rs
@@ -1238,6 +1238,26 @@ impl Workspace {
     }
 
     #[must_use]
+    pub fn take_fullscreen(
+        &mut self,
+    ) -> Option<(
+        Option<FullscreenRestoreState>,
+        Option<Rectangle<i32, Local>>,
+    )> {
+        let surface = self.fullscreen.take()?;
+        if surface.ended_at.is_some() {
+            self.fullscreen = Some(surface);
+            return None;
+        }
+
+        for focus_stack in self.focus_stack.0.values_mut() {
+            focus_stack.retain(|t| t != &surface.surface);
+        }
+
+        Some((surface.previous_state, surface.previous_geometry))
+    }
+
+    #[must_use]
     pub fn remove_fullscreen(
         &mut self,
     ) -> Option<(
