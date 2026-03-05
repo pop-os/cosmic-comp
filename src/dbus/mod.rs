9b40d5d9a726df8936cc40548f3836d9f862c8f4
Ian Douglas Scott <idscott@system76.com>
2026-03-05 15:56:06 -0800

    dbus: Refactor to share DBus connections
    We should avoid creating more than one session connection or more than
    one system connection. We should also ideally avoid blocking the main
    thread.
    
    For now this still uses blocking in a couple places, but wrapping async
    code (which is how `zbus::blocking` is implemented anyway).
    
    This moves the `NameOwners` creation out of `A11yKeyboardMonitorState`,
    so it can be shared with other things. We will likely want that for
    https://github.com/pop-os/cosmic-comp/pull/465 to define a secured
    protocol to pass an fd to the portal, and potentially any other
    sensitive DBus protocols implemented by the compositor.

--- src/dbus/mod.rs
+++ src/dbus/mod.rs
@@ -5,21 +5,110 @@ use crate::{
 use anyhow::{Context, Result};
 use calloop::{InsertError, LoopHandle, stream::StreamSource};
 use cosmic_comp_config::output::comp::OutputState;
-use futures_executor::block_on;
-use std::collections::HashMap;
+use std::{
+    cell::{RefCell, RefMut},
+    collections::HashMap,
+    rc::Rc,
+};
 use tracing::{error, warn};
-use zbus::blocking::{Connection, fdo::DBusProxy};
 
 pub mod a11y_keyboard_monitor;
+use a11y_keyboard_monitor::A11yKeyboardMonitorState;
 #[cfg(feature = "systemd")]
 pub mod logind;
 mod name_owners;
 mod power;
 
-pub fn init(evlh: &LoopHandle<'static, State>) -> Result<()> {
-    match block_on(power::init()) {
+#[derive(Clone, Debug)]
+pub struct DBusState(Rc<DBusStateInner>);
+
+#[derive(Debug)]
+struct DBusStateInner {
+    evlh: LoopHandle<'static, State>,
+    executor: calloop::futures::Scheduler<()>,
+    session_conn: async_once_cell::OnceCell<zbus::Connection>,
+    system_conn: async_once_cell::OnceCell<zbus::Connection>,
+    a11y_keyboard_monitor: RefCell<Option<a11y_keyboard_monitor::A11yKeyboardMonitorState>>,
+}
+
+impl DBusState {
+    pub fn init(evlh: &LoopHandle<'static, State>) -> Self {
+        let (source, executor) = calloop::futures::executor().unwrap();
+        let state = Self(Rc::new(DBusStateInner {
+            evlh: evlh.clone(),
+            executor,
+            session_conn: async_once_cell::OnceCell::new(),
+            system_conn: async_once_cell::OnceCell::new(),
+            a11y_keyboard_monitor: RefCell::new(None),
+        }));
+        evlh.insert_source(source, |_, _, _| {}).unwrap();
+        let state_clone = state.clone();
+        state.spawn(async move {
+            if let Err(err) = init_session(&state_clone).await {
+                tracing::error!("Failed to initialize session DBus connection: {}", err);
+            }
+        });
+        let state_clone = state.clone();
+        state.spawn(async move {
+            if let Err(err) = init_system(&state_clone).await {
+                tracing::error!("Failed to initialize system DBus connection: {}", err);
+            }
+        });
+        state
+    }
+
+    pub fn a11y_keyboard_monitor(
+        &self,
+    ) -> Option<RefMut<'_, a11y_keyboard_monitor::A11yKeyboardMonitorState>> {
+        RefMut::filter_map(self.0.a11y_keyboard_monitor.borrow_mut(), |x| x.as_mut()).ok()
+    }
+
+    async fn session_conn(&self) -> zbus::Result<&zbus::Connection> {
+        self.0
+            .session_conn
+            .get_or_try_init(zbus::Connection::session())
+            .await
+    }
+
+    async fn system_conn(&self) -> zbus::Result<&zbus::Connection> {
+        self.0
+            .system_conn
+            .get_or_try_init(zbus::Connection::system())
+            .await
+    }
+
+    fn spawn(&self, fut: impl Future<Output = ()> + 'static) {
+        let _ = self.0.executor.schedule(fut);
+    }
+}
+
+async fn init_session(state: &DBusState) -> zbus::Result<()> {
+    let conn = state.session_conn().await?;
+    let name_owners = name_owners::NameOwners::new(conn, &state.0.executor).await?;
+    let a11y_keyboard_monitor_state =
+        A11yKeyboardMonitorState::new(conn, &name_owners, &state.0.executor).await?;
+    *state.0.a11y_keyboard_monitor.borrow_mut() = Some(a11y_keyboard_monitor_state);
+    Ok(())
+}
+
+async fn init_system(state: &DBusState) -> zbus::Result<()> {
+    let conn = state.system_conn().await?.clone();
+    let evlh = state.0.evlh.clone();
+    state.spawn(async move {
+        if let Err(err) = power_hot_plug_task(conn, evlh).await {
+            tracing::warn!(?err, "Failed to initialize dbus handlers");
+        }
+    });
+    Ok(())
+}
+
+async fn power_hot_plug_task(
+    conn: zbus::Connection,
+    evlh: LoopHandle<'static, State>,
+) -> Result<()> {
+    match power::init(&conn).await {
         Ok(power_daemon) => {
-            if let Ok(stream) = block_on(power_daemon.receive_hot_plug_detect()) {
+            if let Ok(stream) = power_daemon.receive_hot_plug_detect().await {
                 let source = StreamSource::new(stream).unwrap();
                 evlh.insert_source(source, |_, _, state| {
                     let nodes = match &mut state.backend {
@@ -63,20 +152,22 @@ pub fn init(evlh: &LoopHandle<'static, State>) -> Result<()> {
 /// Updated the D-Bus activation environment with `WAYLAND_DISPLAY` and
 /// `DISPLAY` variables.
 pub fn ready(common: &Common) -> Result<()> {
-    let conn = Connection::session()?;
-    let proxy = DBusProxy::new(&conn)?;
+    futures_executor::block_on(async {
+        let conn = common.dbus_state.session_conn().await?;
+        let dbus = zbus::fdo::DBusProxy::new(conn).await?;
 
-    proxy.update_activation_environment(HashMap::from([
-        ("WAYLAND_DISPLAY", common.socket.to_str().unwrap()),
-        (
-            "DISPLAY",
-            &common
-                .xwayland_state
-                .as_ref()
-                .map(|s| format!(":{}", s.display))
-                .unwrap_or_default(),
-        ),
-    ]))?;
-
+        dbus.update_activation_environment(HashMap::from([
+            ("WAYLAND_DISPLAY", common.socket.to_str().unwrap()),
+            (
+                "DISPLAY",
+                &common
+                    .xwayland_state
+                    .as_ref()
+                    .map(|s| format!(":{}", s.display))
+                    .unwrap_or_default(),
+            ),
+        ]))
+        .await
+    })?;
     Ok(())
 }
