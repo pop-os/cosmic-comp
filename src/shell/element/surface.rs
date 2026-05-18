a16adf0247ead4e97532359106b092934f52aabe
Victoria Brekenfeld <victoria@system76.com>
2026-05-18 17:27:55 +0200

    shell/element: Drop minimize/mapping X11 workaround

--- src/shell/element/surface.rs
+++ src/shell/element/surface.rs
@@ -453,10 +453,6 @@ impl CosmicSurface {
             .store(minimized, Ordering::SeqCst);
         if let WindowSurface::X11(surface) = self.0.underlying_surface() {
             let _ = surface.set_hidden(minimized);
-            if !minimized && surface.is_fullscreen() {
-                let _ = surface.set_mapped(false);
-                let _ = surface.set_mapped(true);
-            }
         }
     }
 
