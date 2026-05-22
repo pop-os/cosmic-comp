b62f3e7c04ba14219dcc03ed34e745be9a2e9dab
Hojjat <hojjat.abdollahi@gmail.com>
2026-05-22 13:03:36 -0600

    fix: update smithay to fix steam input for wayland games

--- src/shell/element/surface.rs
+++ src/shell/element/surface.rs
@@ -342,7 +342,7 @@ impl CosmicSurface {
                     state.is_some_and(|state| state.states.contains(ToplevelState::Resizing))
                 }))
             }
-            WindowSurface::X11(surface) => surface.pending_geometry().map(|_| true),
+            WindowSurface::X11(surface) => surface.pending_configure().map(|_| true),
         }
     }
 
@@ -588,7 +588,7 @@ impl CosmicSurface {
                     }
                 })
             }
-            WindowSurface::X11(surface) => surface.pending_geometry().is_none(),
+            WindowSurface::X11(surface) => surface.pending_configure().is_none(),
         }
     }
 
