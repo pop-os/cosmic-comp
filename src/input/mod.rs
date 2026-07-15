faf87bb8d09f8741725a96c48ac9dff32b4bf724
krakotay <krakotay@yandex.ru>
2026-07-15 14:01:35 +0800

    Fix keycode shortcut classification

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -1960,6 +1960,7 @@ impl State {
 
                 // is this a released (triggered) modifier-only binding?
                 if binding.key.is_none()
+                    && binding.keycode.is_none()
                     && event.state() == KeyState::Released
                     && !cosmic_modifiers_eq_smithay(&binding.modifiers, modifiers)
                     && modifiers_queue.take(binding)
@@ -1973,6 +1974,7 @@ impl State {
 
                 // could this potentially become a modifier-only binding?
                 if binding.key.is_none()
+                    && binding.keycode.is_none()
                     && event.state() == KeyState::Pressed
                     && cosmic_modifiers_eq_smithay(&binding.modifiers, modifiers)
                 {
